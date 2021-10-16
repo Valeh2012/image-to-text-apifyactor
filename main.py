3507231e6715d71611f030ef6bb48109ee8eccec
Valeh2012 <valeh.farzaliyev@cyber.ee>
2021-10-17 02:10:04 +0300

    third try

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ apify_client = ApifyClient(token)
 
 # On Apify platform, input is saved in local Key-Value Store as INPUT record
 kv_store_id = os.getenv('APIFY_DEFAULT_KEY_VALUE_STORE_ID')
-input = json.loads(apify_client.key_value_store(kv_store_id).get_record('INPUT'))
+input = apify_client.key_value_store(kv_store_id).get_record('INPUT')
 #input = {'image_url': './image1.png', 'lang':'en'}
 
 # You have the input now, do stuff with it
 from paddleocr import PaddleOCR
-ocr = PaddleOCR(use_angle_cls=True, lang=input['lang'], use_gpu=False) # need to run only once to download and load model into memory
+ocr = PaddleOCR(use_angle_cls=True, lang=input["lang"], use_gpu=False) # need to run only once to download and load model into memory
 img_path = './imgs_en/img_12.jpg'
-result = ocr.ocr(input['image_url'], cls=True)
+result = ocr.ocr(input["image_url"], cls=True)
 
 output = {}
 i=0
