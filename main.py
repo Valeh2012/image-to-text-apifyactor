87c6c066331b8fd6a576997400afc5e8ed06c29c
Valeh2012 <valeh.farzaliyev@cyber.ee>
2021-10-17 02:42:11 +0300

    fourth try

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 ### First get input
-from apify_client import ApifyClient
+#from apify_client import ApifyClient
 import os
 import json
 
 # Unfortunately, the env vars are only available on the Apify platform
 # For local runs you will have to comment out the client and mock the values
-token = os.getenv('API_TOKEN')
-apify_client = ApifyClient(token)
+#token = os.getenv('API_TOKEN')
+#apify_client = ApifyClient(token)
 
 # On Apify platform, input is saved in local Key-Value Store as INPUT record
-kv_store_id = os.getenv('APIFY_DEFAULT_KEY_VALUE_STORE_ID')
-input = apify_client.key_value_store(kv_store_id).get_record('INPUT')
+#kv_store_id = os.getenv('APIFY_DEFAULT_KEY_VALUE_STORE_ID')
+input = apify_client.key_value_store(kv_store_id).get_record('INPUT')['INPUT']
+
+""" input = {
+  "image_url": "./image1.png",
+  "lang": "en"
+} """
 print(input)
 print(input["image_url"])
 print(input["lang"])
-
-
-#input = {
-#  "image_url": "https://aescripts.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/2/d/2df_simple-text_info_quick-start.png",
-#  "lang": "en"
-#}
 
 # You have the input now, do stuff with it
 from paddleocr import PaddleOCR
@@ -29,11 +28,14 @@ result = ocr.ocr(input["image_url"], cls=True)
 
 output = {}
 i=0
-for line in output:
-    output[i] = line
+for line in result:
+    output[i] = {"bbox": line[0], "text":line[1][0]}
+    i=i+1
+    print(line)
 
-output = json.dumps(output)
 print(output)
+output = json.dumps(output)
+
 datasets = apify_client.datasets()
 if len(datasets.list().items) == 0:
     dataset = apify_client.datasets().get_or_create()
