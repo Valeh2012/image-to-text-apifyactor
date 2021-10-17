072dcfe274fc60e49a8c0949f95c1917e447e26f
Valeh2012 <valeh.farzaliyev@cyber.ee>
2021-10-17 08:52:38 +0300

    cleaner code; tesseract added

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ WORKDIR /app
 
 RUN apt-get update
 RUN apt-get install ffmpeg libsm6 libxext6  -y
+RUN apt-get install latexmk
+RUN apt-get install tesseract-ocr libtesseract-dev libleptonica-dev pkg-config
 
 RUN pip install -r requirements.txt
 
--- main.py
+++ main.py
@@ -1,40 +1,146 @@
-### First get input
 from apify_client import ApifyClient
 import os
 import json
+from PIL import Image
+import requests
+import numpy as np
+import cv2
+import base64
 
-# Unfortunately, the env vars are only available on the Apify platform
-# For local runs you will have to comment out the client and mock the values
 token = os.getenv('API_TOKEN')
 apify_client = ApifyClient(token)
 
-# On Apify platform, input is saved in local Key-Value Store as INPUT record
 kv_store_id = os.getenv('APIFY_DEFAULT_KEY_VALUE_STORE_ID')
 input = apify_client.key_value_store(kv_store_id).get_record('INPUT')["value"]
  
 """ input = {
-  "image_url": "https://aescripts.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/2/d/2df_simple-text_info_quick-start.png",
-  "lang": "en"
-}  """
-print(input)
-print(input["image_url"])
-print(input["lang"])
+  "input_type": "url",
+  "input_image": "https://images4.programmersought.com/934/e8/e89758ae0ed991f1c8aba947addec9e6.png",
+  "lang": "eng",
+  "ocr": "tesseract", # or tesseract
+  "output_format": "txt" # bbox,txt or pdf
+} """
 
+def download_image(img):
+    if img.startswith("http"):
+        res = requests.get(img, stream=True)
+        block_size=1024
+        with open("tmp.jpg", "wb") as file:
+            for data in res.iter_content(block_size):
+                file.write(data)
+        img = "tmp.jpg"
+
+    with open(img, "rb") as file:
+        np_arr = np.frombuffer(file.read(), dtype=np.uint8)
+        image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        return image
+
+
+if input["input_type"] is "url":
+    np_image = download_image(input["input_image"])
+    print(np_image.shape)
+
+elif input["input_type"] is "base64":
+    b64data = input["input_image"].decode("utf-8")
+    raw_image = base64.b64decode(b64data)
+    np_arr = np.frombuffer(raw_image, dtype=np.uint8)
+    np_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+    print(np_image.shape)
+    
+else:
+    output = {"error": "Wrong input type", "response":None}
+
+
+
+if input["ocr"] is "paddle":
 # You have the input now, do stuff with it
-from paddleocr import PaddleOCR
-ocr = PaddleOCR(use_angle_cls=True, lang=input["lang"], use_gpu=False) # need to run only once to download and load model into memory
+    from paddleocr import PaddleOCR
+    from pylatex import Document, MiniPage, TextBlock, MediumText, HugeText, \
+    SmallText, VerticalSpace, HorizontalSpace
+    from pylatex.utils import bold
+    import pylatex.config as cf
 
-result = ocr.ocr(input["image_url"], cls=True)
+    ocr = PaddleOCR(use_angle_cls=False, lang=input["lang"], use_gpu=False) # need to run only once to download and load model into memory
 
-output = {}
-i=0
-for line in result:
-    output[i] = {"bbox": line[0], "text":line[1][0]}
-    i=i+1
-    print(line)
+    result = ocr.ocr(np_image, cls=False)
+
+    if input["output_format"] is "bbox":
+        output = {}
+        i=0
+        for line in result:
+            output[i] = {"bbox": line[0], "text":line[1][0]}
+            i=i+1
+            print(line)
+
+        output = {"response": output, "error":None}
+
+    elif input["output_format"] is "pdf":
+    #pixel_to_inch = 1/72
+        img_width = np_image.shape[1] / 72
+        img_height = np_image.shape[0] / 72
+        print(img_height, img_width)
+
+        cf.active = cf.Version1(indent=False)
+        geometry_options = {"margin": "0in", "paperwidth": "{}in".format(img_width), "paperheight": "{}in".format(img_height)}
+        pdf_export = Document(indent=False, geometry_options=geometry_options)
+        pdf_export.change_length("\TPHorizModule", "1in")
+        pdf_export.change_length("\TPVertModule", "1in")
+        with pdf_export.create(MiniPage(width=r"\textwidth")) as page:
+            for line in result:
+                with page.create(TextBlock((line[0][1][0]-line[0][0][0])/72,line[0][0][0] / 72, line[0][0][1] / 72)):
+                    page.append(line[1][0])
+
+        pdf_export.generate_pdf("pdf_export", clean_tex=False)
+        #os.rename("./pdf_export.tex", "./tex_export.tex")
+
+
+        output = None
+        with open("pdf_export.pdf", "rb") as file:
+            output = base64.b64encode(file.read())
+
+        output = {"response": output.decode('utf-8'), "error":None}
+        
+
+    elif input["output_format"] is "txt":
+        output = {"error": "Paddle OCR does not support txt output", "response":None}
+    
+    else:
+        output = {"error": "Unsupported output format", "response":None}
+
+
+elif input["ocr"] is "tesseract":
+    import subprocess
+
+    cv2.imwrite("./tmp.jpg", np_image)
+
+    if input["output_format"] is "txt":
+        subprocess.run(["tesseract","./tmp.jpg", "output", "-l", "{}".format(input["lang"])])
+        output_txt = None
+        with open("output.txt", "r") as file:
+            output_txt = file.read()
+        output = {"response": output_txt, "error":None}
+
+    elif input["output_format"] is "pdf":
+        subprocess.run(["tesseract", "./tmp.jpg", "output", "-l", "{}".format(input["lang"]), "pdf"])
+        output = None
+        with open("output.pdf", "rb") as file:
+            output = base64.b64encode(file.read())
+
+        output = {"response": output.decode('utf-8'), "error":None}
+    
+    elif input["output_format"] is "json":
+        output = {"error": "Tesseract OCR does not support bbox output", "response":None}
+    
+    else:
+        output = {"error": "Unsupported output format", "response":None}
+
+
+else:
+    output = {"error": "Unsupported OCR", "response":None}
 
 print(output)
 output = json.dumps(output)
+
 
 datasets = apify_client.datasets().list()
 if len(datasets.items) == 0:
